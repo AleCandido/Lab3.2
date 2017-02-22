d7100cf6a481666c4762344daf7e2364e1f55278
Andreo95 <andrea.luzio95@gmail.com>
2017-02-22 16:53:30 +0100

    little security patch

--- Utilities/util.py
+++ Utilities/util.py
@@ -3,34 +3,8 @@ import lab
 import uncertainties
 from operator import *
 
-#todo sarebbe meglio avere una sola, è codice identico...ma ci sono problemi con i <=... che in fondo sono solo altre fun...
+defoult_hwindow=30
 
-def oldfindLocalMaxs(data, hwindow):
-    '''
-    trova i massimi assoluti delle sottofinestre di semilarghezza hwindow
-    data:   dati, np array
-    hwindow:    intero, semilarghezza della finestra (larghezza finestra = 2*hwindow+1)
-    return lista indici massimi
-    '''
-    maxs=[]
-    for i in range(hwindow, len(data)-hwindow):
-        if(data[i]>np.amax(data[i-hwindow: i]) and data[i]>=np.amax(data[i+1: i+hwindow+1])):
-           maxs.append(i)
-    return maxs
-
-
-def oldfindLocalMins(data, hwindow):
-    '''
-    trova i minimi assoluti delle sottofinestre di semilarghezza hwindow
-    data:   dati, np array
-    hwindow:    intero, semilarghezza della finestra (larghezza finestra = 2*hwindow+1)
-    return lista indici massimi
-    '''
-    maxs=[]
-    for i in range(hwindow, len(data)-hwindow):
-        if(data[i]<np.amin(data[i-hwindow: i]) and data[i]<=np.amin(data[i+1: i+hwindow+1])):
-           maxs.append(i)
-    return maxs
 
 def findLocal(data, hwindow, f1, f2, f3):
     maxs=[]
@@ -39,7 +13,7 @@ def findLocal(data, hwindow, f1, f2, f3):
            maxs.append(i)
     return maxs
 
-findLocalMaxs=lambda data, hwindow: findLocal(data, hwindow, np.amax, gt, ge)
+findLocalMaxs=(lambda x: lambda data, hwindow: x(data, hwindow, np.amax, gt, ge))(findLocal) #si mi piace supercazzolare, e si, è diverso...
 findLocalMaxs.__doc__=    '''
     trova i massimi assoluti delle sottofinestre di semilarghezza hwindow
     data:   dati, np array
@@ -47,7 +21,7 @@ findLocalMaxs.__doc__=    '''
     return lista indici massimi
     '''
 
-findLocalMins=lambda data, hwindow: findLocal(data, hwindow, np.amax, lt, le)
+findLocalMins=(lambda x: lambda data, hwindow: x(data, hwindow, np.amin, lt, le))(findLocal) 
 findLocalMins.__doc__= '''
     trova i minimi assoluti delle sottofinestre di semilarghezza hwindow
     data:   dati, np array
@@ -56,12 +30,13 @@ findLocalMins.__doc__= '''
     '''
 
 
-def BetterFindLocal(ydata, dydata, hwindow=5, f1, f2):
+def BetterFindLocal(ydata, dydata, hwindow, f1, f2):
     '''approssima i massimi come parabole e con questo cerca il vero massimo. Se fornita degli errori sulle y fa un lavoretto migliore....'''
     massimi=[]
     candidati=f1(ydata, hwindow)
+    p=lambda x, A, B, C: A*x**2+B*x+C
     for i in candidati:
-        par, covs = lab.curve_fit(lambda x, A, B, C: A*x**2+B*x+C, np.array(range(-hwindow, hwindow)), ydata[i-hwindow: i+hwindow], sigma=dydata)
+        par, covs = lab.curve_fit(p, np.array(range(-hwindow, hwindow)), ydata[i-hwindow: i+hwindow], sigma=dydata)
         corpar=uncertainties.correlated_values(par, covs)
         A, B, C=corpar
         if(f2(A, 0)):
@@ -69,13 +44,8 @@ def BetterFindLocal(ydata, dydata, hwindow=5, f1, f2):
         massimi.append((i,ydata[i], par, covs, -B/2*A, -B**2/(4*A)+C))
     return massimi
 
-BetterFindLocalMaxs=lambda ydata, dydata, hwindow=5: BetterFindLocal(ydata, dydata, hwindow, findLocalMaxs, ge)
-BetterFindLocalMins=lambda ydata, dydata, hwindow=5: BetterFindLocal(ydata, dydata, hwindow, findLocalMins, le)
 
+#be, così non ci si capisce più nulla no!!!
+BetterFindLocalMaxs=(lambda x, y:lambda ydata, dydata, hwindow=defoult_hwindow: y(ydata, dydata, hwindow, x, ge))(findLocalMaxs, BetterFindLocal)
+BetterFindLocalMins=(lambda x, y:lambda ydata, dydata, hwindow=defoult_hwindow: y(ydata, dydata, hwindow, x, le))(findLocalMins, BetterFindLocal)
 
-
-
-
-    
-    
-    
