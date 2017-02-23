9e15f0bdbf7a9e99d4f5ff6bd9a711d5b3c09a99
AleCandido <candido.ale@gmail.com>
2017-02-23 13:33:35 +0100

    altre cose

--- 1_Franck_Hertz/Esercitazione.py
+++ 1_Franck_Hertz/Esercitazione.py
@@ -1,7 +1,7 @@
 from pylab import *
 
 import getpass
-users={"candi": "C:\\Users\\alessandro\\Documents\\GitHub\\Lab3.2\\",
+users={"candi": "C:\\Users\\candi\\Documents\\GitHub\\Lab3.2\\",
 "silvanamorreale":"C:\\Users\\silvanamorreale\\Documents\\GitHub\\Lab3.2\\" ,
 "Studenti": "C:\\Users\\Studenti\\Desktop\\Lab3\\",
 "User":"C:\\Users\\User\\Documents\\GitHub\\Lab3.2\\"
@@ -32,17 +32,47 @@ def diffe(l, g=lambda x: x[4]):
             return
 
 
+
+
+
 for i in range(5, 6):
     file=dir+"csv\\Task9.{}.csv".format(i)
     o=OscilloscopeData(file)
-    o.plot()
-    maxs=util.BetterFindLocalMaxs(o.CH2, o.dCH2)#[o.CH1>0], o.dCH2[o.CH1>0])
     
-    diffe1=list(diffe(maxs))
-    print(diffe1)
-    mins=util.BetterFindLocalMins(o.CH2, o.dCH2)
-    print(mins)
-    diffe2=list(diffe(mins))
-    print("maxsl={}, minl={}".format(len(maxs), len(mins)))
-    for i in range(4):
-        print("\n\r")
+    #fittiamo la retta...
+    rdatas=o.CH1[o.CH1>0]
+    ddatas=o.dCH1[o.CH1>0]
+    rdatas=rdatas[0: len(rdatas)-1]
+    ddatas=ddatas[0: len(rdatas)]
+    print(ddatas)
+    par, covs=lab.fit_affine_xerr(rdatas, range(rdatas),[0.4])    
+    print(par, covs)
+    m, q=uncertainties.correlated_values(par, covs)
+    M=1/m
+    Q=-q/m
+    
+    print(M)
+    print(Q)
+    mm=M.n
+    qq=Q.n
+    dom=np.array(range(len(rdatas)))
+    pylab.figure(0)
+    pylab.plot(dom, rdatas)
+    pylab.plot(dom, (lambda x: mm*x+qq)(dom))
+    
+    
+    maxs=util.BetterFindLocalMaxs(o.CH2, o.dCH2)#, o.dCH2)#
+    for max in maxs: 
+        print((max[4], max[5]))
+    
+   
+   
+   #  
+   #  diffe1=list(diffe(maxs))
+   #  print(diffe1)
+   #  mins=util.BetterFindLocalMins(o.CH2, o.dCH2)
+   #  print(mins)
+   #  diffe2=list(diffe(mins))
+   #  print("maxsl={}, minl={}".format(len(maxs), len(mins)))
+   #  for i in range(4):
+   #      print("\n\r")
