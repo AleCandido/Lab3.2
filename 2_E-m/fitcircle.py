da230bc746b352e1674cbbb09fe6c56bb7fa8a24
AleCandido <candido.ale@gmail.com>
2017-03-01 15:48:13 +0100

    derivate

--- 2_E-m/fitcircle.py
+++ 2_E-m/fitcircle.py
@@ -4,7 +4,7 @@ import math
 from scipy.optimize import curve_fit
 
 #per le notazioni guardare il file pdf 'circle fit'
-x,y= pylab.loadtxt( 'C:\\Users\\User\\Documents\\GitHub\\Lab3.2\\2_E-m\\data\\12.txt' , unpack = True)
+x,y= pylab.loadtxt( 'C:\\Users\\candi\\Documents\\GitHub\\Lab3.2\\2_E-m\\data\\12.txt' , unpack = True)
 
 dx = 1
 dy = 1
@@ -21,27 +21,33 @@ my = sum(y)/len(y)
 u = x-mx
 v = y-my
 suu = sum(u**2)
-suuu = sum(u**3)
 suv = sum(u*v)
-suvv = sum(u*(v**2))
 svv = sum(v**2)
+suuu = sum(u**3)
+suvv = sum(u*(v**2))
+suuv = sum(v*(u**2))
 svvv = sum(v**3)
-svuu = sum(v*(u**2))
 
 # risolvo sistema per trovare coordinate centro circonferenza
-det = suu*svv - suv**2
-det1 = (0.5*(suuu+suvv)*svv)-(0.5*suv*(svvv+svuu))
-det2 = (0.5*suu*(svvv+svuu))-(0.5*(suuu+suvv)*suv)
+det = suv**2 - suu*svv
+det1 = ((svvv+suuv)*suv - (suuu+suvv)*svv)/2
+det2 = ((suuu+suvv)*suv - (svvv+suuv)*suu)/2
 uc = det1/det
 vc= det2/det
 xc= uc + mx
 yc= (vc + my)
 print('xc =', xc)
 print('yc =', yc)
-R = math.sqrt((uc**2) + (vc**2) + (suu+svv)/len(x))
+r = math.sqrt((uc**2) + (vc**2) + (suu+svv)/len(x))
 pylab.plot(xc,yc, marker = 'o')
-print('R=', R)
-x = numpy.linspace(min(x)-10, max(x)+10,1000)
-y = numpy.sqrt(R**2-(x-xc)**2) +yc
+print('r=', r)
+
+x = numpy.linspace(xc - r + 1, xc + r -1, 1000)
+y = numpy.sqrt(r**2-(x-xc)**2) +yc
 pylab.plot(x,y, color = 'black')
+
+x1 = numpy.linspace(xc - r + 1, xc + r -1, 1000)
+y1 = - numpy.sqrt(r**2-(x-xc)**2) +yc
+pylab.plot(x1,y1, color = 'black')
+
 pylab.show()
