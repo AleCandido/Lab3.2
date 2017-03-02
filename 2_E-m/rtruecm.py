332754d4a407aaa911eb339a609cb5dda46ab2d0
AleCandido <candido.ale@gmail.com>
2017-03-02 22:58:27 +0100

    1

--- 2_E-m/rtruecm.py
+++ 2_E-m/rtruecm.py
@@ -1,3 +1,11 @@
+import os
+import lab
+import pylab
+import numpy as np
+import uncertainties
+import sys
+import scipy.stats
+import scipy.integrate
 from uncertainties import *
 from scipy.integrate import quad
 
@@ -30,21 +38,60 @@ icoil = [ufloat(0,0) for i in range(0,len(preicoil))]
 for i in range(0,len(preicoil)):
     icoil[i] = ufloat(preicoil[i],0.01)
     
-def field(r, R, z0, I):
-    r=np.abs(r)+epsilon
-    A=(R**2+r**2+z0**2)
-    fun=lambda teta: R*(R-r*np.cos(teta))/(A-2*R*r*np.cos(teta))**3/2
-    return I*scipy.integrate.quad(fun,0 ,2*np.pi)[0] 
+## Correzione prospettica
     
 rtt = [0 for i in range(0,len(rtls))]
 
 for i in range(0,len(rtls)):
     rtt[i] = rtls[i]*d/D
 
+## Campo  magnetico
+
+def field(r, R, z0, I):
+    r=np.abs(r)+epsilon
+    A=(R**2+r**2+z0**2)
+    fun=lambda teta: R*(R-r*np.cos(teta))/(A-2*R*r*np.cos(teta))**3/2
+    return I*scipy.integrate.quad(fun,0 ,2*np.pi)[0] 
+    
+# derivate
+
+epsilon=1e-4
+def DfieldDr(r, I, a, z, R, O, errorscale=epsilon):
+    return (field(r+espilon, I, a, z, R, O)-field(r, I, a, z, R, O))/epsilon
+
+def DfieldDI(r, I, a, z, R, O, errorscale=epsilon):
+    return (field(r, I+epsilon, a, z, R, O)-field(r, I, a, z, R, O))/epsilon
+    
+def DfieldDa(r, I, a, z, R, O, errorscale=epsilon):
+    return (field(r, I, a+epsilon, z, R, O)-field(r, I, a, z, R, O))/epsilon
+    
+def DfieldDz(r, I, a, z, R, O, errorscale=epsilon):
+    return (field(r, I, a, z+epsilon, R, O)-field(r, I, a, z, R, O))/epsilon
+    
+def DfieldDR(r, I, a, z, R, O, errorscale=epsilon):
+    return (field(r, I, a, z, R+epsilon, O)-field(r, I, a, z, R, O))/epsilon
+    
+def DfieldDO(r, I, a, z, R, O, errorscale=epsilon):
+    return (field(r, I, a, z, R, O+epsilon)-field(r, I, a, z, R, O))/epsilon
+
+def Derive(f, i, epsilon=1e-6):
+    def derivata(*pars):
+        mypars=list(pars)
+        mypars[i]+=epsilon
+        return (f(*mypars)-f(*pars))/epsilon
+    return derivata
+    
+# errore sul campo magnetico
+    
+def fieldErrorSq(x, dx, pars, dpars):
+    return (dx**2)*Derive(field, 0)(x,*pars)**2+sum(dpars[i]*(Derive(field, 1+i)(r,*pars))**2 for i in range(0, len(pars))) 
+
 B = [ufloat(0,0) for i in range(0,len(icoil))]
 
 for i in range(0,len(preicoil)):
     B[i] = ufloat(field(rtt, R0, R0/2, icoil), dfield())
+    
+## Calcolo di e/m
     
 em = [ufloat(0,0) for i in range(0,len(B))]
 
