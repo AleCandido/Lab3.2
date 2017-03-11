b3a30b6333ac6bd9db811c248ca695a5caf40399
AleCandido <candido.ale@gmail.com>
2017-03-11 15:43:33 +0100

    figura

--- 4_Ottica1/cadmio.py
+++ 4_Ottica1/cadmio.py
@@ -1,6 +1,9 @@
 from lab import *
 from pylab import *
 from uncertainties import *
+from uncertainties import unumpy
+from statistics import *
+from scipy.constants import *
 
 import getpass
 users={"candi": "C:\\Users\\candi\\Documents\\GitHub\\Lab3.2\\",
@@ -23,12 +26,40 @@ dir= path + "4_Ottica1\\"
 
 alphar = loadtxt(dir + "data\\cadmio.txt", unpack=True)
 
-lamda = array([ 467.8, 480.0, 508.6, 643.8])*10**(-9)
-nu = 1/lamda
-
-alpha0 = ufloat(10.895, 0.026)
+alphas = alphar[0] + alphar[1]/60
 
 alpha = []
 dalpha = []
 
-alpha += [0]
+for i in range(0, len(alphas), 3):
+    alpha += [mean(alphas[i:i+3])]
+    err = stdev(alphas[i:i+3])
+    if err != 0:
+        dalpha += [err]
+    else:
+        dalpha += [1.5/60]
+
+alphal = unumpy.uarray(alpha,dalpha)
+
+alpha0 = ufloat(10.895, 0.026)
+alphai = alphal - alpha0
+
+lamda = array([467.8, 480.0, 508.6, 643.8])*10**(-9)
+E = (h/physical_constants['electron volt'][0])*c/lamda
+
+errorbar(unumpy.nominal_values(alphai), E, xerr = unumpy.std_devs(alphai), fmt = 'b,')
+
+par, cov = fit_linear(E, unumpy.nominal_values(alphai), dy = unumpy.std_devs(alphai))
+
+m, q = par
+
+x = linspace(min(E)*0.99, max(E)*1.01)
+y = m*x + q
+
+xlim(min(y)*0.998, max(y)*1.002)
+xlabel("Deflection angles [°]")
+ylabel("Light energies [eV]")
+
+plot(y, x, 'g')
+
+savefig(dir + "grafici\\calcadmio.pdf")
