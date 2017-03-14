5d15852ee5903fcafa52f690b119d4510c105987
FabrizioMazziotti <fabrymazziotti@gmail.com>
2017-03-14 16:58:41 +0100

    Calibro, retry

---
+++ 3_Ottica2/MichelsonMorley.py
@@ -0,0 +1,24 @@
+from pylab import *
+from lab import *
+from uncertainties import *
+from uncertainties import unumpy
+
+import getpass
+users={"candi": "C:\\Users\\candi\\Documents\\GitHub\\Lab3.2\\",
+"silvanamorreale":"C:\\Users\\silvanamorreale\\Documents\\GitHub\\Lab3.2\\" ,
+"Studenti": "C:\\Users\\Studenti\\Desktop\\Lab3\\",
+"User":"C:\\Users\\User\\Documents\\GitHub\\Lab3.2\\"
+}
+try:
+    user=getpass.getuser()
+    path=users[user]
+    print("buongiorno ", user, "!!!")
+except:
+    raise Error("unknown user, please specify it and the path in the file Esercitazione*.py")
+
+
+sys.path = sys.path + [path]
+dir= path + "3_Ottica2\\"
+
+##
+
