7f2deae92262c3db8d21b9a6e102d703785301e8
AleCandido <candido.ale@gmail.com>
2017-03-22 01:42:26 +0100

    correnti input

--- Esercitazione10/Esercitazione10.py
+++ Esercitazione10/Esercitazione10.py
@@ -15,14 +15,33 @@ except:
 
 
 sys.path = sys.path + [path]
-dir= path + "Esercitazione/"
+dir= path + "Esercitazione10\\"
 
 from BuzzLightyear import * 
-import uncertainties
+from lab import mme
+from uncertainties import *
 ###########################################################################
 
 file="Iin"
 
-loadtxt(dir + file + ".txt", unpack=True)
+vin, iin = loadtxt(dir + "data\\" + file + ".txt", unpack=True)
+
+dvin = mme(vin, 'volt')
+diin = mme(iin, 'ampere', 'ice680')
+
+iin *= 1
+diin *= 1
+
+xlim(min(vin)*0.9,max(vin)*1.03)
+ylim(-max(iin)/20,max(iin)*1.1)
+errorbar(vin, iin, xerr=dvin, yerr=diin, fmt=",b")
+
+xlabel("Input voltages [V]")
+ylabel("Input currents [mA]")
+
+savefig(dir + "grafici\\" + file + ".pdf")
+
+# ticklabel_format(style='sci', axis='both', scilimits=(0,0))
+
 
 ###########################################################################
