4de2bbd04fd6a58b9861b5c4e4b398860532d933
AleCandido <candido.ale@gmail.com>
2017-03-29 19:55:19 +0200

    sincronizzazione

---
+++ Esercitazione11/Esercitazione11.py
@@ -0,0 +1,28 @@
+from pylab import *
+
+import getpass
+users={"candi": "C:\\Users\\candi\\Documents\\GitHub\\Lab3.2\\",
+"silvanamorreale":"C:\\Users\\silvanamorreale\\Documents\\GitHub\\Lab3.2\\" ,
+"Studenti": "C:\\Users\\Studenti\\Desktop\\Lab3\\",
+"User":"C:\\Users\\User\\Documents\\GitHub\\Lab3.2\\"
+}
+try:
+    user=getpass.getuser()
+    path=users[user]
+    print("buongiorno ", user, "!!!")
+except:
+    raise Error("unknown user, please specify it and the path in the file Esercitazione*.py")
+
+
+sys.path = sys.path + [path]
+dir= path + "Esercitazione/"
+
+from BuzzLightyear import * 
+import uncertainties
+###########################################################################
+
+file="Iin"
+
+loadtxt(dir + file + ".txt", unpack=True)
+
+###########################################################################
