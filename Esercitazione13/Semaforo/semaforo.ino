41018de61cd6ee43144058d3a248017feace7b3f
Andreo95 <andrea.luzio95@gmail.com>
2017-05-08 19:12:48 +0200

    ora è come sarebbe scritto davvero un programma c, va bene la chiarezza, ma non esageriamo!!!

--- Esercitazione13/Semaforo/semaforo.ino
+++ Esercitazione13/Semaforo/semaforo.ino
@@ -2,6 +2,8 @@
  *  Create a traffic light with FSM on arduino
  */
 #define SERIALE 0 //se la trasmissione seriale degli stati è abilitata
+#define ENABLE_LOGICAL_HIGH HIGH
+
 
 #define LVstate 0
 #define LVGstate 1
@@ -9,18 +11,18 @@
 #define LOFFstate 3
 #define LGstate 4
 
+#define LVpin  9 // led verde
+#define LGpin  10 // led giallo
+#define LRpin  11 // led rosso
+#define ENpin 8 // input pin for enable
 
-const int LVpin = 9; // led verde
-const int LGpin = 10; // led giallo
-const int LRpin = 11; // led rosso
-const int ENpin = 8; // input pin for enable
+#define timeV  4000 //time of greeen (ms)
+#define timeVG  1500 //time of verde giallo (ms)
+#define timeR  2000 //time of rosso (ms)
+#define timeG   1500 // time del lampeggiante
 
-const int timeV = 4000; //time of greeen (ms)
-const int timeVG = 1500; //time of verde giallo (ms)
-const int timeR = 2000; //time of rosso (ms)
-const int timeG =  1500; // time del lampeggiante
-
-int times[4];
+int pins[4]={LVpin, LGpin, LRpin, ENpin};
+int times[4]={timeV, timeVG, timeR, timeG};
 int outputs[5][3]={{HIGH, LOW, LOW},{HIGH, HIGH, LOW},{LOW, LOW, HIGH},{LOW, LOW, LOW} , {LOW, HIGH, LOW}}; //tabella degli output in funzione dello stato corrente!!!
 
 
@@ -36,50 +38,35 @@ void setup() {
   pinMode(LGpin, OUTPUT);
   pinMode(LRpin, OUTPUT);
   pinMode(ENpin, INPUT_PULLUP);
-	//setup tempi...
-  times[0]=timeV;  //questo non sarebbe proprio necessario....ma va be!
-  times[1]=timeVG;
-  times[2]=timeR;
-  times[3]=timeG; 
 }
 
 void loop() {
-  // 1. read the input
-  // swith (state) 
-  // 2. generate the output
-  // 3. calculate the next state
-  // 4. wait for this state to complete
-  //1.
-  inEnable = (digitalRead(ENpin)==HIGH);
+  //1. read the input
+  inEnable = (digitalRead(ENpin)==ENABLE_LOGICAL_HIGH);
  #if SERIALE 
   Serial.println(thisState);
   Serial.println(inEnable);
  #endif 
   stateWait=times[thisState];
-  //2.
+  //2.generate the output
   setOutput(outputs[thisState]);
-  //3.
+  //3.calculate the next state
   if(inEnable) nextState=(thisState+1)%3; //loop fra gli stati 0, 1, 2. se trova 3 va in 1, se trova 4 va in 2
   else nextState=3+ thisState % 2; //loop fra gli stati 3-4. se trova 0 o 2 va in 3, se trova 1 va in 4
-  //4.
+  //4. wait for this state to complete
   delay(stateWait);
   thisState = nextState;
 }
 
 
 int setOutput(int* ordine) {
-  int LV=ordine[0];
-  int LG=ordine[1];
-  int LR=ordine[2];
  #if SERIALE 
   Serial.println("setOutput");
-  Serial.print(LV);  
-  Serial.print(LG);
-  Serial.println(LR);
+  for(int i=0; i<3; i++)
+    Serial.println(ordine[i]);
  #endif 
-  digitalWrite(LVpin, LV);  
-  digitalWrite(LGpin, LG);
-  digitalWrite(LRpin, LR);
+ for(int i=0; i<3; i++)
+    digitalWrite(pins[i] , ordine[i]);
 }
 
 
